b1be58bc47bb02cb540ad917c343b28608a7abd6
Kaioru <keifuurawrs@gmail.com>
2017-09-10 09:53:32 +0800

    Resolve #2

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@
 const Deskly = require('../lib/Deskly')
 const pkg = require('../package.json')
 const fs = require('fs')
+const util = require('util')
 const commander = require('commander')
 const wallpaper = require('wallpaper')
 const got = require('got')
@@ -15,74 +16,38 @@ const conf = new Configstore(pkg.name, {
   limit: 50
 })
 
-let d = new Deskly(conf.all)
+let d = new Deskly()
 let dir = __dirname + '/generated/'
 
 commander
   .version(pkg.version)
 
 commander
-  .command('add-subreddit <subreddit>')
-  .action((subreddit) => {
-    let array = conf.get('subreddits')
-    array.push(subreddit)
-    conf.set('subreddits', array)
-  })
-
-commander
-  .command('remove-subreddit <subreddit>')
-  .action((subreddit) => {
-    let array = conf.get('subreddits')
-    let index = array.indexOf(subreddit);
-    if (index > -1) {
-      conf.set('subreddits', array)
-    }
-  })
-
-commander
-  .command('sort-type <type>')
-  .action((type) => {
-    conf.set('sort', type)
-  })
-
-commander
-  .command('show-config')
-  .action(() => {
-    console.log(conf.all)
-  })
-
-commander
-  .command('clear-config')
-  .action(() => {
-    conf.clear()
-  })
-
-commander
   .command('generate')
   .description('Generates a new desktop image.')
-  .action(() => {
-    console.log('Searching for wallpapers..'.green)
-    d.getPost().then(post => {
-      console.log('Downloading ' + post.title.cyan + ' by ' + post.author.cyan + ' from r/' + post.subreddit.cyan)
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-      }
+  .option("-r, --subreddit <subreddit>", "Specify the subreddit to use.")
+  .option("-s, --sort <sort>", "Specify the sort-type to use.")
+  .option("-l, --limit <limit>", "Specify the limit of posts fetched.")
+  .action((options) => {
+    let def = conf.all
+    let sort = options.sort || def.sort
+    let limit = options.limit || def.limit
+    let subreddit = options.subreddit || d.getRandomSubreddit(def.subreddits)
+    let promise = d.getPosts(sort, limit, subreddit)
+
+    console.log(util.format('Searching for wallpapers on r/%s..', subreddit.green))
+
+    d.getPost(promise).then(post => {
       let path = dir + post.id + '.jpg'
+
+      console.log(util.format('Downloading %s by %s', post.title.green, post.author.green))
+      if (!fs.existsSync(dir)) fs.mkdirSync(dir)
       got.stream(post.url)
         .pipe(fs.createWriteStream(path))
-        .on('finish', function() {
+        .on('finish', () => {
           wallpaper.set(path)
-          console.log('Aaand we\'re done! Image saved to ' + path.cyan)
+          console.log(util.format('Aand we\'re done! Saved image to %s', path.green))
         })
-    })
-  })
-
-commander
-  .command('path')
-  .description('Returns the path of your current desktop image.')
-  .action(() => {
-    wallpaper.get().then(path => {
-      console.log(path);
     })
   })
 
